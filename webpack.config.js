eab3059b1984741e25bdb8359bd511590520cde2
FlameV3 <FlameX.D@skiff.com>
2023-10-07 06:10:13 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,57 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const InlinePlugin = require("html-inline-script-webpack-plugin")
-const path = require("path")
-const fs = require('fs')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const InlinePlugin = require("html-inline-script-webpack-plugin");
+const path = require("path");
+const fs = require("fs");
 
-let plugins = [
-	new HtmlWebpackPlugin({
-		template: './src/index.html',
-		inject: "body",
-	})
-]
+// Define an array to hold your Webpack plugins
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    inject: "body",
+  }),
+];
 
-// Build to the dist folder by default
-let output = "./dist"
+// Set the default output path to the "dist" folder
+let output = "./dist";
 
-// If I'm coding on my VPS, build to the beta page on willard.fun
+// Check for specific development environments
 if (__dirname.includes("willard/server/dev")) {
-	output = "../../public/minekhan/beta"
+  // If coding on your VPS, build to the beta page on willard.fun
+  output = "../../public/minekhan/beta";
+} else if (__dirname.includes("/home/willard/Desktop/MineKhan")) {
+  // If coding on your desktop and have your VPS mounted, build to the beta page on willard.fun
+  const vpsPath = "/home/willard/vps";
+  if (fs.existsSync(vpsPath) && fs.readdirSync(vpsPath).includes("server")) {
+    output = path.join(vpsPath, "server/public/minekhan/beta");
+  }
 }
 
-// If I'm coding on my desktop, and have my VPS mounted, build to the beta page on willard.fun
-else if (__dirname.includes("/home/willard/Desktop/MineKhan")) {
-	if (fs.readdirSync("/home/willard/vps").includes("server")) {
-		output = "/home/willard/vps/server/public/minekhan/beta"
-	}
-}
-
-// Bundle the JS into the HTML file; makes debugging harder, and breaks webpack's auto build
-// plugins.push(new InlinePlugin())
-
+// Create and export the Webpack configuration
 module.exports = {
-	mode: "none",
-	entry: "./src/main.js",
-	output: {
-		path: path.resolve(__dirname, output),
-		filename: "main.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.(txt|glsl)$|workers\//i,
-				use: "raw-loader",
-			},
-		],
-	},
-	plugins,
-	watch: true,
-	watchOptions: {
-		aggregateTimeout: 100,
-		// poll: 1000,
-		ignored: '**/node_modules',
-	},
-}
+  mode: "none",
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, output),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(txt|glsl)$|workers\//i,
+        use: "raw-loader",
+      },
+    ],
+  },
+  plugins,
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 100,
+    // Uncomment the following line if you want to poll for changes
+    // poll: 1000,
+    ignored: '**/node_modules',
+  },
+};
