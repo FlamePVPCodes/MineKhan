eebb89ddf1842872c7e1c7602b89456c58292df5
FlameV3 <FlameX.D@skiff.com>
2023-10-07 06:03:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-// This is for testing on localhost
-const express = require("express")
-const app = express()
-app.listen(4000)
+// Import the required modules
+const express = require("express");
+const app = express();
+
+// Define the port number
+const port = 4000;
+
+// Middleware to enable CORS and serve static files
 app.use('/', (req, res, next) => {
-	// Make SharedArrayBuffer work
-	res.header('Cross-Origin-Opener-Policy', 'same-origin')
-	res.header('Cross-Origin-Embedder-Policy', 'require-corp')
-	next()
-})
-app.use(express.static('dist'))
+    // Allow SharedArrayBuffer
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    next();
+});
+
+// Serve static files from the 'dist' directory
+app.use(express.static('dist'));
+
+// Start the server
+app.listen(port, () => {
+    console.log(`🔥 Server is up and running on port ${port}! 🔥`);
+});
